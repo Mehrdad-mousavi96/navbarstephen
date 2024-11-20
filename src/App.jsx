f64ef17857e4be5d0394dbba61d2af6d53bbc576
Mehrdad <mehrdad.fmousavi@gmail.com>
2024-11-20 12:11:25 +0330

    we only defined Button component and we will create button branch to save changes inside that branch

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Button from "./Button";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <Button />
+      </div>
+      <div>
+        <Button />
+      </div>
+      <div>
+        <Button />
+      </div>
+      <div>
+        <Button />
+      </div>
+      <div>
+        <Button />
+      </div>
+    </div>
+  );
+};
+
+export default App;
